b9a6e9d992b6d588c4229a6c078172e841934e86
TheBond08 <101583061+TheBond08@users.noreply.github.com>
2022-11-22 16:19:40 +0200

    Update make-latex.sh

--- make-latex.sh
+++ make-latex.sh
@@ -2,12 +2,12 @@
 #assemble and preprocess all the sources files
 
 if [ ! -d "./latex" ]; then
-   echo "  creating missing directory for(latex)"
+   echo "  creating missing directory latex"
    mkdir ./latex
 fi
 
 if [ ! -d "./book" ]; then
-   echo "  creating missing directory for(book)"
+   echo "  creating missing directory book"
    mkdir ./book
 fi
 
@@ -19,25 +19,25 @@ pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-lev
 
 for filename in text/ch*.txt; do
       [ -e "$filename" ] || continue
-      echo "  $filename -> latex/$(basename "$filename" .txt).tex..."
+      echo " Converting $filename to latex/$(basename "$filename" .txt).tex"
       pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=contribution.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"    
    done
 
-echo "  text/epi.txt -> latex/epi.tex..."
+echo " Converting text/epi.txt to latex/epi.tex"
 pandoc text/epi.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/epi.tex
 
 for filename in text/apx*.txt; do 
    [ -e "$filename" ] || continue
-   echo "  $filename -> latex/$(basename "$filename" .txt).tex..."
+   echo " Converting $filename to latex/$(basename "$filename" .txt).tex"
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"   
 done
 
-echo " Merging the .tex files into a single one. Please standby, it will take a few minutes."
+echo " Commencing the merger of the .tex files. Please wait."
 pandoc -s latex/*.tex -o book/book.tex
 
-echo "  Creating the .pdf book from the .tex files. Please wait."
+echo " Starting the creation of the .pdf book from the .tex files."
 pandoc -N --quiet --variable "geometry=margin=1.2in" --variable mainfont="MesloLGS NF Regular" --variable sansfont="MesloLGS NF Regular" --variable monofont="MesloLGS NF Regular" --variable fontsize=12pt --variable version=2.0 book/book.tex  --pdf-engine=xelatex --toc -o book/book.pdf
 
-echo "  The creation of the .tex & .pdf file has finished!  ;)"
+echo " The .tex & .pdf files have been finalized!"
 
 #sed -i '' 's+Figure+Εικόνα+g' ./latex/ch0*
